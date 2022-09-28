4f27780b155ef6ed16dd2ce31699d4575e51373d
davidhartsough <hartsoughdavid@gmail.com>
2022-09-29 00:01:50 +1000

    improve JSDoc comments on initDatabase()

--- src/firebase-init.ts
+++ src/firebase-init.ts
@@ -1,18 +1,19 @@
 import { initializeApp, getApps, FirebaseOptions } from "firebase/app";
 
 /**
- * Initialize the Firebase app / database
+ * Initialize the Firebase app / database.
+ * Example config parameter:
  * const config = {
  *   apiKey: "YOUR_API_KEY",
  *   authDomain: "your-app.firebaseapp.com",
  *   projectId: "your-app",
  *   storageBucket: "your-app.appspot.com",
- *   messagingSenderId: "80085",
- *   appId: "1:80085:web:80085",
+ *   messagingSenderId: "12345",
+ *   appId: "1:12345:web:12345",
  *   measurementId: "G-ASDF",
  * };
- * @param config FirebaseOptions
- * @returns boolean
+ * @param {FirebaseOptions} config FirebaseOptions
+ * @returns {boolean} boolean
  */
 export function initDatabase(config: FirebaseOptions): boolean {
   if (getApps().length === 0) {
